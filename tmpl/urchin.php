837d2681d3e1e63483a402b87a8f1bc0126613ac
Dean Tedesco <dino@tedesco.net.au>
2008-03-12 10:42:25 +0000

    mod_janalytics initial check in of version 1.0.0 files.
    git-svn-id: http://joomlacode.org/svn/mod_janalytics/trunk@1 296c2a57-2948-0410-8eba-fae360a3e4ad

---
+++ tmpl/urchin.php
@@ -0,0 +1,9 @@
+<?php // no direct access
+defined('_JEXEC') or die('Restricted access');
+?>
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "<?php echo $analyticsId; ?>";
+urchinTracker();
+</script>
