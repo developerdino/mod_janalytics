3426a08234e491445f65136e824ba587aa4de69e
Dean Tedesco <dino@tedesco.net.au>
2009-03-05 08:41:13 +0000

    Fixed svn:keywords property.
    git-svn-id: http://joomlacode.org/svn/mod_janalytics/trunk@22 296c2a57-2948-0410-8eba-fae360a3e4ad

--- tmpl/urchin.php
+++ tmpl/urchin.php
@@ -1,28 +1,28 @@
 <?php
-# J!Analytics - Google Analytics Code module for Joomla v1.5
-#
-# @version		$Id$
-# @author		Dean Tedesco <dino@tedesco.net.au>
-# @link		http://www.tedesco.net.au
-# @copyright 	Copyright (C) 2008 Dean Tedesco. All rights reserved.
-# @license 	http://www.gnu.org/copyleft/gpl.html GNU/GPL
-# This file is part of J!Analytics.
-# 
-# J!Analytics is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# J!Analytics is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with J!Analytics.  If not, see <http://www.gnu.org/licenses/>.
-?>
+/**
+ * J!Analytics - Google Analytics Code module for Joomla v1.5
+ *
+ * @version		$Id$
+ * @author		Dean Tedesco <dino@tedesco.net.au>
+ * @link		http://www.tedesco.net.au
+ * @copyright 	Copyright (C) 2008 Dean Tedesco. All rights reserved.
+ * @license 	http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * This file is part of J!Analytics.
+ * 
+ * J!Analytics is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * J!Analytics is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with J!Analytics.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-<?php // no direct access
 defined('_JEXEC') or die('Restricted access');
 ?>
 <script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
